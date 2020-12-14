6e36c21b4f3c51c0a586d3e2bcd0334eb485ae8c
Allangr-eab <allangabrielrvsantos@yahoo.com.br>
2020-12-13 22:24:02 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console . Clear ();
+            Console . WriteLine ( " --- Temperatura do conversor --- " );
+
+            Console . Write ( " Informe a temperatura em ° F: " );
+            double  f  =  Convert . ToDouble ( Console . ReadLine ());
+
+            duplo  c  = ( f  -  32 ) /  1,8 ;
+            Console . WriteLine ( $ " ° F = { f } " );
+            Console . WriteLine ( $ " { f } ° F equivalente a { c } ° C " );
+            Console . WriteLine ( " Pressione qualquer tecla para sair " );
+            Console . ReadKey ();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GrausFC
+
+**Enunciado**:
+
+Converta uma temperatura digitalada pelo usuário em °C para °F.
+
+**Exemplo**:
+
+°F = °C × 1,8 + 32
+
+Ref.: https://pt.wikipedia.org/wiki/Celsius
+
+°C = 0°C equivalomi a 32°F
